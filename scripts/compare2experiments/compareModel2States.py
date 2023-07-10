7cb2e110a8939373d557e64539fc741d80db9419
Daniel Garza <danielriosgarza@gmail.com>
2023-07-10 16:18:30 +0200

    updates

--- scripts/compare2experiments/compareModel2States.py
+++ scripts/compare2experiments/compareModel2States.py
@@ -401,6 +401,87 @@ stTypes = ['cells',
 figPath = os.path.join(Path(os.getcwd()).parents[1], 'files', 'Figures', species+'Experiments')
 for i,v in enumerate(states):
     makeExperimentPlot(species, v, stTypes[i], experiments, labels, colors, simulObj = [bhri, None, None], alpha=0.5)
+    #plt.savefig(os.path.join(figPath, v + '_model.png'), dpi = 150)
+    #plt.savefig(os.path.join(figPath, 'logos', v + '_model.png'), dpi = 50)
+    plt.show()
+    
+    
+####################################################################################
+
+species = 'btri'
+experiments = ['btri']
+labels = ['btri']
+colors = ['#003eff']
+
+#params = getPramsFromFile('ri', os.path.join(Path(os.getcwd()).parents[1], 'files', 'params', 'ri.tsv'))
+
+databaseName = 'modelDB_bhbtri_bh.sqlite3'
+
+databaseFolder =  os.path.join(Path(os.getcwd()).parents[1], 'files', 'dbs')
+
+database = os.path.join(databaseFolder, databaseName)
+
+#conn = create_connection(os.path.join(databaseFolder, databaseName))
+#assignRiParams(params, conn)
+
+
+
+
+
+
+measuredStates = ['live_bt',
+                  'live_ri',
+          
+            
+          'pyruvate',
+          'glucose',
+          'acetate',
+          'lactate',
+          'butyrate',
+          'succinate',
+          'formate'
+          ]
+
+btri = simulateExperiment(group = 'btri', 
+                       experimentLabel = 'btri', 
+                       dbPath = database, 
+                       measuredStates = measuredStates
+                       )
+
+
+states = ['live_bt',
+          'live_ri',
+          'dead',
+          
+          'pH',
+          
+          
+        'pyruvate',
+        'glucose',
+        'acetate',
+        'lactate',
+        'butyrate',
+        'succinate',
+        'formate']
+
+
+stTypes = ['cells',
+            'cells',
+            'cells',
+            'pH',
+            
+            'metabolite',
+            'metabolite',
+            'metabolite',
+            'metabolite',
+            'metabolite',
+            'metabolite',
+            'metabolite'
+            ]
+
+figPath = os.path.join(Path(os.getcwd()).parents[1], 'files', 'Figures', species+'Experiments')
+for i,v in enumerate(states):
+    makeExperimentPlot(species, v, stTypes[i], experiments, labels, colors, simulObj = [btri, None, None], alpha=0.5)
     plt.savefig(os.path.join(figPath, v + '_model.png'), dpi = 150)
     plt.savefig(os.path.join(figPath, 'logos', v + '_model.png'), dpi = 50)
     plt.show()
